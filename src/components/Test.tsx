b790bd684900a344d98755eb0a495a5e88cbfd28
Bùi Vũ Anh <vuanhlk12@gmail.com>
2022-10-13 15:28:51 +0700

    Initial commit

---
+++ src/components/Test.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import Logo from "../icons/favicon.svg?component";
+
+const Test: FC = (props) => {
+  return (
+    <div>
+      <Logo />
+      1234
+    </div>
+  );
+};
+
+export default Test;
