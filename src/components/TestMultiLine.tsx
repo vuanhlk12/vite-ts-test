5acaa223f94eaf45ef500a0682112df9507b0676
Bùi Vũ Anh <vuanhlk12@gmail.com>
2023-11-19 12:23:32 +0700

    s

--- src/components/TestMultiLine.tsx
+++ src/components/TestMultiLine.tsx
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import "./TestMultiLine.scss";
-
+import converter from "jp-conversion";
+   
 const TestMultiLine = () => {
   const [count, setCount] = useState([0]);
   console.log(count);
+  var res = converter.convert("しんかんせん");
+  console.log("res", res);
   return (
     <div className="container">
       <button onClick={() => setCount([...count, count.length + 1])}>
